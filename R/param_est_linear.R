118971747c608964d4df9bdcd47445b7dd4693b6
Justin Petrovich <jpetrovich02@gmail.com>
2018-05-15 23:18:39 -0400

    a bunch of changes so that k gets passed from the misfit arguments into the param_est_linear function, and then into the model for fcr

--- R/param_est_linear.R
+++ R/param_est_linear.R
@@ -24,15 +24,17 @@
 #'@export
 
 
-param_est_linear <- function(dat,workGrid,cond.y=TRUE,fcr.args = list(use_bam = T,niter = 1),
-                             k=15,nPhi = NULL,face.args=list(knots = 12, pve = 0.95)){
+param_est_linear <- function(dat,workGrid,cond.y=TRUE,fcr.args = list(use_bam = T,niter = 1),k = 15,nPhi = NULL,
+                             face.args=list(knots = 12, pve = 0.95)){
   N <- length(unique(dat[,"subj"]))
+  # cat("k is ",deparse(substitute(k)), " deparsed")
   # fit <- NULL
   if(cond.y){
     # nPhi <- min(c(floor((nrow(dat) - 2*k)/N),J))
     ks <- deparse(substitute(k))
-    if(!is.null(nPhi)){fcr.args['nPhi'] <- deparse(nPhi)}
+    if(!is.null(nPhi)){fcr.args['nPhi'] <- nPhi}
     rhs <- paste("~ ", "s(argvals, k =", ks,", bs = \"ps\") + s(argvals, by = y, k =", ks,", bs = \"ps\")")
+    # rhs <- paste("s(argvals, k =", ks,", bs = \"ps\") + s(argvals, by = y, k =", ks,", bs = \"ps\")")
     # model <- reformulate(response = "X",termlabels = rhs)
     model <- update.formula(rhs, "X ~ .")
     # model <- update(~ s(argavls, k = k, bs = "ps") + s(argvals, by = y, k = k, bs = "ps"),X ~ .)
